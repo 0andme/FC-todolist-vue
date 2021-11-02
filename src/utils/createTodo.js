d57d2ca1bdfa510f08be50f135a55a8b0809a15d
0andme <yu00mi97@gmail.com>
2021-11-03 06:05:38 +0900

    feat : createTodo / TODO 항목 추가 함수 생성

---
+++ src/utils/createTodo.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+export async function createTodo(title,order) {
+  const { data } = await axios({
+    url: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'apikey': 'FcKdtJs202110',
+      'username': 'YouYoungMi'
+    },
+    data: {
+      title,
+      order
+    }
+  })
+  //console.log(data)
+  return data
+}
