8679c1b6f75c50996c36e35735a754c4beec88dd
0andme <yu00mi97@gmail.com>
2021-11-03 06:26:17 +0900

    feat : TodoInput 컴포넌트 추가
    - todo리스트의 title부분을 사용자로부터 입력받는다
    - 플러스 버튼이나 엔터를 입력하면 새로운 todo아이템생성

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="inputTodoItem">
+    <!-- 입력창 -->
+    <input
+      type="text"
+      placeholder="Add your new Todo"
+      v-model="newTodoItemTitle"
+      @keyup.enter="addTodoItem"/>
+    <!-- add 버튼-->
+    <button @click="addTodoItem" class="inputBtn">
+      <i class="fas fa-plus"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+import {createTodoItem}from "~/utils/createTodoItem"
+
+export default {
+  props:{
+    todoNum:{
+      type:Number,
+      required : true 
+    }
+  },
+  data(){
+    return{
+      newTodoItemTitle:''
+    }
+  },
+  methods:{
+    addTodoItem(){
+      // 공백이 아닐 때
+      if(this.newTodoItemTitle!=='') 
+      {
+        createTodoItem(this.newTodoItemTitle,this.todoNum)
+          .then(()=>{
+            location.reload();
+          })
+        this.newTodoItemTitle="" //입력값 초기화
+      }
+      //입력받은 값이 공백일때
+
+    }
+  }
+  
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
