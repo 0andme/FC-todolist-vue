60279c4b1c3a8d3881d1313987b36c0c1fbc2cdd
0andme <yu00mi97@gmail.com>
2021-11-03 06:33:18 +0900

    fix : 함수에 대한 주석 추가

--- src/utils/putTodo.js
+++ src/utils/putTodo.js
@@ -1,5 +1,7 @@
+// 하나의 아이템을 통채로 가져와 객체 분해
+// 받아온 아이디를 가진 아이템을 가져온 제목,체크여부,순서로 내용 변경
+// 수정된 객체를 반환
 import axios from "axios"
-
 
 export async function putTodo({id,title,done,order}) {
   const { data } = await axios({
@@ -17,5 +19,5 @@ export async function putTodo({id,title,done,order}) {
     }
   })
   
-  console.log(data)
+  //console.log(data)
 }
