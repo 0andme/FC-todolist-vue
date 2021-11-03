e9cbdc67682cbf32987ff0492bcd536ef176d001
0andme <yu00mi97@gmail.com>
2021-11-03 20:18:22 +0900

    feat : 리스트의 항목을 컨트롤하는 TodoListController 컴포넌트 추가
    - 리스트의 항목을 전부 삭제하는 컨트롤러 기능 추가

---
+++ src/components/TodoListController.vue
@@ -0,0 +1,29 @@
+<template>
+<!-- 정렬 -->
+<!-- 리스트 전부 삭제 -->
+<button @click="clearAllListItem">ClearAll</button>
+  
+</template>
+<script>
+import {deleteTodo} from '~/utils/deleteTodo'
+
+export default {
+  props:{
+    controlList:{
+      type:Array,
+      required : true 
+    }
+  },
+  methods:{
+    clearAllListItem(){
+      
+      console.log(this.controlList)
+      this.controlList.map(todo=>deleteTodo(todo.id).then(()=>{location.reload();}))
+      // location.reload();
+          
+
+    }
+  }
+  
+}
+</script>
