0265ef31c8911e40ddec5b09ecbf5f7d812ca1fd
0andme <yu00mi97@gmail.com>
2021-11-03 09:11:49 +0900

    feat : TodoListItem 컴포넌트 생성
    구성
    - 체크박스
    - 항목의 내용
    - 항목 수정버튼
    - 항목 삭제버튼
    - 항목 수정 컴포넌트

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,63 @@
+<template>
+<label >
+  <input type="checkbox" :checked="isCheck" @change="todochecked(todo)" class="doList__checkBox">
+  <span v-if="!isEditMode">{{todo.title}}</span>
+</label>
+<!-- 수정 삭제 버튼 -->
+<button class="doList__btn--edit" v-if="!isEditMode" @click="isEditMode=true">수정</button>
+<button class="doList__btn--delete" v-if="!isEditMode" @click="delTodoItem(todo.id)">
+  <i class="fas fa-times"></i>
+</button>
+<!-- 수정 입력 컴포넌트 -->
+  <TodoListEdit v-if="isEditMode" :todo="todo" @isEditMode="isEditMode=$event"/>
+
+</template>
+
+<script>
+import {deleteTodo,putTodo }from "~/utils/index"
+import TodoListEdit from '~/components/TodoListEdit.vue'
+
+export default {
+  components:{
+    TodoListEdit
+  },
+  props:{
+    isCheck:{
+      type:Boolean,
+      required:true
+      
+    },
+    todo:{
+      type:Object,
+      required : true 
+      
+    }
+  },
+  data(){
+    return{
+      isEditMode:false
+      
+    }
+  },
+  methods:{
+    todochecked(todo){
+      todo.done=!todo.done
+      putTodo(todo)
+        .then(()=>{
+              location.reload();
+            })  
+      
+    },
+    delTodoItem(todoId){
+      deleteTodo(todoId)
+        .then(()=>{
+              location.reload();
+            })
+    }
+  }
+  
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
