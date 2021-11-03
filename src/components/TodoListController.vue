138d2a13fe0456872c218bef53d018993eb155f2
0andme <yu00mi97@gmail.com>
2021-11-04 06:25:23 +0900

    feat : 주석 추가 / 항목 전부 삭제 함수 변경

--- src/components/TodoListController.vue
+++ src/components/TodoListController.vue
@@ -1,3 +1,8 @@
+// TodoListController 
+// 기능 : 받아온 리스트 항목 삭제
+// 받아온 리스트가 dolist, donelist 일 수 있음
+// ClearAll버튼이 눌리면 받아온 리스트 항목의 id를 통해 항목을 삭제
+
 <template>
 <!-- 정렬 -->
 <!-- 리스트 전부 삭제 -->
@@ -5,7 +10,7 @@
   
 </template>
 <script>
-import {deleteTodo} from '~/utils/deleteTodo'
+import {deleteAllTodo} from '~/utils/index'
 
 export default {
   props:{
@@ -16,14 +21,17 @@ export default {
   },
   methods:{
     clearAllListItem(){
-      
-      console.log(this.controlList)
-      this.controlList.map(todo=>deleteTodo(todo.id).then(()=>{location.reload();}))
-      // location.reload();
-          
-
+      if(this.controlList.length>0)
+      { 
+        // console.log("test1")
+        
+        deleteAllTodo(this.controlList)
+        .then((msg)=>{
+          console.log('end')
+        })
+      // console.log("test2")
+      }
     }
   }
-  
 }
 </script>
