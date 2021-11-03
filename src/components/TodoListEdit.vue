f0d2cb3424187247dec5c1adfc68eaa24a65d0c7
0andme <yu00mi97@gmail.com>
2021-11-03 09:13:24 +0900

    feat : TodoListEdit 컴포넌트 생성
    구성
    - 입력 받기 위한 input 태그
    - 입력 확인 혹은 취소를 위한 버튼

---
+++ src/components/TodoListEdit.vue
@@ -0,0 +1,57 @@
+<template>
+<!-- 수정을 위한 입력과 버튼 -->
+<input
+  type="text"
+  placeholder="Edit Todo"
+  v-model="editTodoTitle"
+  @keyup.enter="editTodoItem"/>
+<button class="doList__btn--edit"  @click="editedItem" >완료</button>
+<button class="doList__btn--edit" @click="$emit('isEditMode',false)">취소</button>
+  
+</template>
+<script>
+import {putTodo} from '~/utils/putTodo'
+
+export default {
+  props:{
+    isEditMode:{
+      type:Boolean,
+      required : true 
+
+    },
+    todo:{
+      type:Object,
+      required : true 
+      
+    }
+  },
+  data(){
+    return{
+      editTodoTitle:'',
+      editedTodo:{title:''}
+      // ,
+      // isEditMode=this.isEditMode
+
+    }
+  },
+  created(){
+    this.editedTodo={...this.todo}
+    this.editTodoTitle=this.todo.title 
+  },
+
+  methods:{
+    editedItem(){
+      this.editedTodo.title=this.editTodoTitle
+      putTodo(this.editedTodo)
+        .then(()=>{
+              location.reload();
+            })      
+    }
+
+  }
+  
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
