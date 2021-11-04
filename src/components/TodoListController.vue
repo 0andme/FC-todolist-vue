2caa15b5ac3622f097013c3d93797af6eec8ebf5
0andme <yu00mi97@gmail.com>
2021-11-04 18:36:40 +0900

    feat : 스타일을 위한 요소 추가

--- src/components/TodoListController.vue
+++ src/components/TodoListController.vue
@@ -4,9 +4,14 @@
 // ClearAll버튼이 눌리면 받아온 리스트 항목의 id를 통해 항목을 삭제
 
 <template>
-<!-- 정렬 -->
+<!-- 정렬 기능 미구현-->
 <!-- 리스트 전부 삭제 -->
-<button @click="clearAllListItem">ClearAll</button>
+<div class="controlBox">
+<button class="clearBtn" @click="clearAllListItem">ClearAll</button>
+
+</div>
+
+
   
 </template>
 <script>
@@ -28,6 +33,11 @@ export default {
         deleteAllTodo(this.controlList)
         .then((msg)=>{
           console.log('end')
+          // location.reload()
+          // 위의 코드가 비동기로 잘 작동되지 않아
+          // 새로고침 버튼을 화면에 추가 했는데 
+          // 동작이 안 되는 이유를 알고 싶어요
+
         })
       // console.log("test2")
       }
