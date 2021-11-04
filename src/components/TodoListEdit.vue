98d6210e98cb38dff9d6d45d6903ed49adddf1c2
0andme <yu00mi97@gmail.com>
2021-11-04 16:11:27 +0900

    feat : 버튼요소에  fontawesome요소 추가 / 스타일을 위한 박스요소 추가

--- src/components/TodoListEdit.vue
+++ src/components/TodoListEdit.vue
@@ -1,14 +1,18 @@
 <template>
 <!-- 수정을 위한 입력과 버튼 -->
 <!-- Extraneous non-emits event listeners warning 해결을 위한 div-->
-<div> 
+<div class="editBox"> 
   <input
   type="text"
   placeholder="Edit Todo"
   v-model="editTodoTitle"
   @keyup.enter="editTodoItem"/>
-  <button class="doList__btn--edit"  @click="editedItem" >완료</button>
-  <button class="doList__btn--edit" @click="this.$emit('cancelEditMode',false)">취소</button>
+  <button class="listItem__btn edited"  @click="editedItem" >
+    <i class="fas fa-check"></i>
+  </button>
+  <button class="listItem__btn cancel" @click="this.$emit('cancelEditMode',false)">
+    <i class="fas fa-times"></i>
+  </button>
 </div>
 </template>
 <script>
@@ -46,6 +50,3 @@ export default {
   
 }
 </script>
-<style lang="scss" scoped>
-
-</style>
