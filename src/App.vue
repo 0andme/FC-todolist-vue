2ce6d14532099005ad157281e58b078bce0c281d
0andme <yu00mi97@gmail.com>
2021-11-06 15:37:53 +0900

    feat : 로딩 애니메이션 추가

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
 <!-- 헤더 -->
 <TodoHeader/>
-
-<div class="todoBox">
+<Loader v-if='!fetched'/>
+<div v-if='fetched' class="todoBox">
   <TodoItemInput />
   <TodoListBox/>
 </div>
@@ -16,16 +16,25 @@ import TodoListBox from '~/components/TodoListBox'
 import TodoItemInput from '~/components/TodoItemInput'
 import TodoHeader from '~/components/TodoHeader'
 import TodoFooter from '~/components/TodoFooter'
+import Loader from '~/components/Loader'
 
 export default {
   components:{
     TodoListBox,
     TodoHeader,
     TodoItemInput,
-    TodoFooter
+    TodoFooter,
+    Loader
+  },
+  data(){
+    return {
+      fetched:false
+    }
   },
   created(){
-    this.$store.dispatch('tododata/getTodoList')
+    this.$store.dispatch('tododata/getTodoList').then(()=>{
+      this.fetched=true
+    })
   }
 }
 </script>
---
+++ src/components/Loader.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="loader">
+    <div class="loader__item"></div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.loader{
+  width: 70%;
+  position: absolute;
+  top: 50%;
+  left: 50;
+  transform: translate(25%,25%);
+  display: flex;
+  justify-content: center;
+.loader__item {
+  $loaderSize: 80px;
+  width: $loaderSize;
+  height: $loaderSize;
+  left: 50%;
+
+  margin-top: calc(-#{$loaderSize}/2);
+  margin-left: calc(-#{$loaderSize}/2);
+
+  z-index: 1;
+  border-width: 20px;
+  border-style: solid;
+  border-color:#fff;
+  border-radius: 50%;
+  border-top-color: transparent;
+
+  animation: loading-spin .8s linear infinite;
+
+}
+
+@keyframes loading-spin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+}
+</style>
